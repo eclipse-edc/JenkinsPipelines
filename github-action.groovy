715f846eead8f214e596ecb3bde09390b495894c
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-11-30 16:29:55 +0100

    use variables

--- github-action.groovy
+++ github-action.groovy
@@ -1,6 +1,8 @@
 pipeline {
     agent any
     environment {
+        OWNER='eclipse-edc'
+        REPO='GradlePlugins'
         WORKFLOW= 'test.yaml'
     }
     stages {
@@ -8,7 +10,7 @@ pipeline {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'github-bot', passwordVariable: 'BOTTOKEN', usernameVariable: 'BOT')]) {
                     sh (script:
-                            '''curl --fail -X POST -u $BOT:$BOTTOKEN -H \'Accept: application/vnd.github.v3+json\' -d \'{"ref":"main"}\' "https://api.github.com/repos/eclipse-edc/GradlePlugins/actions/workflows/test.yaml/dispatches"''',
+                            '''curl --fail -X POST -u $BOT:$BOTTOKEN -H \'Accept: application/vnd.github.v3+json\' -d \'{"ref":"main"}\' "https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$WORKFLOW/dispatches"''',
                             returnStdout: true)
                 }
             }
