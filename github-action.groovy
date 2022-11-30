e109655bae77d291837409539be3f8702836543c
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-11-30 15:59:38 +0100

    proper escaping

--- github-action.groovy
+++ github-action.groovy
@@ -7,11 +7,9 @@ pipeline {
         stage("setup-credentials") {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'github-bot', passwordVariable: 'BOTTOKEN', usernameVariable: 'BOT')]) {
-                    sh (script: '''curl --fail -sSL -X POST \\
-                        -u $BOT:$BOTTOKEN \\
-                        -H "Accept: application/vnd.github.v3+json" \\
-                        -d '{"ref":"main"}' \\
-                        https://api.github.com/repos/eclipse-edc/GradlePlugins/actions/workflows/${WORKFLOW}/dispatches''', returnStdout: true)
+                    sh (script:
+                            '''curl --fail -X POST -u $BOT:$BOTTOKEN -H \'Accept: application/vnd.github.v3+json\' -d \'{"ref":"main"}\' "https://api.github.com/repos/eclipse-edc/GradlePlugins/actions/workflows/test.yaml/dispatches"''',
+                            returnStdout: true)
                 }
             }
         }
