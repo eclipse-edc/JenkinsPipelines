45fc3e9b3bd0b284e2cb668c0e2c0a26741abe73
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-12-01 13:45:22 +0100

    split waiting for a job and for completion into 2 scripts

--- github-action.groovy
+++ github-action.groovy
@@ -1,6 +1,9 @@
+def run_id = ''
+
 pipeline {
     agent any
     environment {
+        // TODO: convert these into job parameters
         OWNER = 'eclipse-edc'
         REPO = 'GradlePlugins'
         WORKFLOW = 'test.yaml'
@@ -13,11 +16,24 @@ pipeline {
                 }
             }
         }
-        stage("wait-for-job-created") {
+        stage("wait-until-job-created") {
+            steps {
+                timeout(30) {
+                    withCredentials([usernamePassword(credentialsId: 'github-bot', passwordVariable: 'BOTTOKEN', usernameVariable: 'BOT')]) {
+                        // this will create a file "run.id" that contains the run-id
+                        sh './scripts/wait_job_created.sh $OWNER $REPO $WORKFLOW $BOT $BOTTOKEN'
+                    }
+                }
+            }
+        }
+        stage("wait-until-job-completed") {
             steps {
                 timeout(60) {
                     withCredentials([usernamePassword(credentialsId: 'github-bot', passwordVariable: 'BOTTOKEN', usernameVariable: 'BOT')]) {
-                        sh './scripts/wait_for_build.sh $OWNER $REPO $WORKFLOW $BOT $BOTTOKEN'
+                        script {
+                            run_id = readFile('run.id').trim()
+                        }
+                        sh './scripts/wait_job_completed.sh $OWNER $REPO $run_id $BOT $BOTTOKEN'
                     }
                 }
             }
--- scripts/wait_job_completed.sh
+++ scripts/wait_job_completed.sh
@@ -2,7 +2,7 @@
 
 OWNER="$1"
 REPO="$2"
-WORKFLOW="$3"
+RUN_ID="$3"
 USER="$4"
 PWD="$5"
 
@@ -10,43 +10,26 @@ if [ "$#" -eq 4 ]; then
   # use cURL with a Personal Access Token
   echo "Using USER as personal access token for the GitHub API"
   PARAMS=(-H "Authorization: Bearer $USER" -H "Accept: application/vnd.github.v3+json")
-
-elif
-  [ "$#" -eq 5 ]
-then
+elif [ "$#" -eq 5 ]; then
   # use basic auth with cUrl
   echo "Using USER/PWD authentication for the GitHub API"
   PARAMS=(-u "$USER":"$PWD" -H "Accept: application/vnd.github.v3+json")
-
 else
-  echo "Usage: start_build.sh OWNER REPO WORKFLOW USER [PWD]"
+  echo "Usage: wait_job_completion.sh OWNER REPO RUN_ID USER [PWD]"
   echo "OWNER    = the owner/org of the github repo"
   echo "REPO     = the name of the github repo"
-  echo "WORKFLOW = the name of the workflow file to run, or its ID"
+  echo "RUN_ID   = the ID of an (existing) workflow run"
   echo "USER     = the username to use for authentication against the GitHub API, or an API token"
   echo "PWD      = the password of USER. if not specified, USER will be interpreted as token"
   exit 1
 fi
 
-numRuns=0
-echo "Waiting for run to start"
-while [ "$numRuns" -le "0" ]; do
-  # fetch the latest run triggered by a workflow_dispatch event
-  runs=$(curl --fail -sSl "${PARAMS[@]}" -X GET "https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW}/runs?event=workflow_dispatch&status=in_progress")
-  numRuns=$(echo "$runs" | jq -r '.total_count')
-  echo " - found $numRuns"
-  sleep 3
-done
-
-# contains the ID of the latest/most recent run
-runId=$(echo "$runs" | jq -r '.workflow_runs[0].id')
-
-echo "Checking run ID $runId"
+echo "Checking run ID $RUN_ID"
 while [ "$status" != "completed" ]; do
-  json=$(curl --fail -sSl "${PARAMS[@]}" -X GET "https://api.github.com/repos/${OWNER}/${REPO}/actions/runs/${runId}")
+  json=$(curl --fail -sSl "${PARAMS[@]}" -X GET "https://api.github.com/repos/${OWNER}/${REPO}/actions/runs/${RUN_ID}")
   status=$(echo "$json" | jq -r '.status')
   conclusion=$(echo "$json" | jq -r '.conclusion')
-  echo "$(date) :: $runId is $status"
+  echo "$(date) :: Run $RUN_ID is $status"
   sleep 5
 done
 
