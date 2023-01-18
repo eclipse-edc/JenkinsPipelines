6966a56d7afa90075c88b06964daa06f843c8515
ndr_brt <andrea.bertagnolli@gmail.com>
2023-01-18 09:57:17 +0100

    Commented out discord webhook

--- release_all.groovy
+++ release_all.groovy
@@ -33,9 +33,10 @@ pipeline {
         }
 
     }
-    post {
-        always {
-            build(job: "../DiscordWebhook", parameters: [string(name: "UPSTREAM_JOB_URL", value: "${env.BUILD_URL}"), string(name: "JOB_NAME", value: "${env.JOB_NAME}"), string(name: "BUILD_NUMBER", value: "${env.BUILD_NUMBER}"), string(name: "REPO_URL", value: "https://github.com/eclipse-edc/Connector"), string(name: "CONTENT", value: "Look, I built a nightly version ${VERSION} of the components!"), string(name: "WEBHOOK_URL", value: "https://discord.com/api/webhooks/1044851842754027622/vLIhAy_eTODGk5GZbsYJSV351bQVJPvCGpw57AeJxqIVJ0eN11tlH8FCj3HayXjefyzz")])
-        }
-    }
+    // dismissed because this webhook was being used maliciously, that's why discord disabled it. We need to put the url in jenkins credentials
+    // post {
+    //     always {
+    //         build(job: "../DiscordWebhook", parameters: [string(name: "UPSTREAM_JOB_URL", value: "${env.BUILD_URL}"), string(name: "JOB_NAME", value: "${env.JOB_NAME}"), string(name: "BUILD_NUMBER", value: "${env.BUILD_NUMBER}"), string(name: "REPO_URL", value: "https://github.com/eclipse-edc/Connector"), string(name: "CONTENT", value: "Look, I built a nightly version ${VERSION} of the components!"), string(name: "WEBHOOK_URL", value: "https://discord.com/api/webhooks/1044851842754027622/vLIhAy_eTODGk5GZbsYJSV351bQVJPvCGpw57AeJxqIVJ0eN11tlH8FCj3HayXjefyzz")])
+    //     }
+    // }
 }
