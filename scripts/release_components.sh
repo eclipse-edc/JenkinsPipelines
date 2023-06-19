78b1064b2d39aa5622b22ffbd92de42abb92943b
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2023-06-19 16:24:20 +0200

    fix: replace comma with colon to pass valid JSON

--- scripts/release_components.sh
+++ scripts/release_components.sh
@@ -10,28 +10,28 @@ set -eu
 
 pids=()
 
-./scripts/github_action.sh "eclipse-edc" "gradleplugins" "release-all-java.yml" "{\"metamodel_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "gradleplugins" "release-all-java.yml" "{\"metamodel_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
-./scripts/github_action.sh "eclipse-edc" "connector" "release-edc.yml" "{\"edc_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "connector" "release-edc.yml" "{\"edc_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
-./scripts/github_action.sh "eclipse-edc" "identityhub" "release-identityhub.yml" "{\"ih_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "identityhub" "release-identityhub.yml" "{\"ih_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
-./scripts/github_action.sh "eclipse-edc" "registrationservice" "release-registrationservice.yml" "{\"rs_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "registrationservice" "release-registrationservice.yml" "{\"rs_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
-./scripts/github_action.sh "eclipse-edc" "federatedcatalog" "release-fcc.yml" "{\"edc_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "federatedcatalog" "release-fcc.yml" "{\"edc_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
-./scripts/github_action.sh "eclipse-edc" "technology-azure" "release-tech-az.yml" "{\"edc_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "technology-azure" "release-tech-az.yml" "{\"edc_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
-./scripts/github_action.sh "eclipse-edc" "technology-aws" "release-tech-aws.yml" "{\"edc_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "technology-aws" "release-tech-aws.yml" "{\"edc_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
-./scripts/github_action.sh "eclipse-edc" "technology-gcp" "release-tech-gcp.yml" "{\"edc_version\", \"${VERSION}\"}" $BOT $BOTTOKEN &
+./scripts/github_action.sh "eclipse-edc" "technology-gcp" "release-tech-gcp.yml" "{\"edc_version\": \"${VERSION}\"}" $BOT $BOTTOKEN &
 pids+=($!)
 
 # Wait for workflow completion, if any of them fails, the script will fail.
